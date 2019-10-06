84aaabc27ef7ff4aa0c20a01519acd7e28464ef3
root <root@pop-os.localdomain>
2019-10-06 16:04:11 +0700

    navbar

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import logo from './logo.svg';
 import './App.css';
 // Page
 import Home from './containers/Home';
+import NavigationBar from './containers/NavigationBar';
 
 function App() {
   return (
     <div className="App">
+      <NavigationBar/>
       <Home />
     </div>
   );
---
+++ src/containers/NavigationBar/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navbar from '../../components/NavbarComponent';
+
+const NavigationBar = () => {
+    return (
+        <div>
+        <Navbar/>
+        </div>
+    );
+}
+
+export default NavigationBar;
