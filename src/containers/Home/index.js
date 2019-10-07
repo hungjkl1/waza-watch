a895e374ed52ac7879a42281eaf15b5075e0a915
ThanhTam27 <thanhtam180298@gmail.com>
2019-10-07 11:02:09 +0700

    Update Home page

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,65 +1,96 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Col, Row, Container } from 'react-bootstrap';
+import { Col, Row, Container, Card, Button } from 'react-bootstrap';
 // Image
 import image from './image.json';
 // Style
 import './home.scss';
-
-const Home = () => {
+const handleClick = () => {
+  console.log("Hello")
+}
+const Home = (props) => {
   return (
     <div>
       <Container>
         <Row>
-          <Col sm={12} md={6}>
-            <div className='big-image-container'>
-              <img src={image.bigImage[0].url}
-                alt="example"
-                width='100%' />
-            </div>
-          </Col>
-          <Col sm={12} md={6}>
-            <div className='big-image-container'>
-              <img src={image.bigImage[1].url}
-                alt="example"
-                width='100%' />
-            </div>
-          </Col>
-        </Row>
-
-        <Row>
           {
-            image.smallImage.map((item)=>{
-              return(
-                <Col xs={6} sm={6} md={3}>
-                <div className='small-image-container'>
-                  <img src={item.url}
-                    alt="example"
-                    width='100%' />
-                </div>
-              </Col>
+            image.bigImage.map((item) => {
+              return (
+                <Col sm={12} md={6}>
+                  <div className='big-image-container'>
+                    <img src={item.url} onClick={handleClick}
+                      alt="example"
+                      width='100%' />
+                    <h1 className="big-image-container__title">{item.title}</h1>
+                  </div>
+                </Col>
               )
             })
           }
         </Row>
 
-        <div className='example-image-container'>
-          <Row>
-            <Col sm={6} md={3}>
-              <div className='example-image-container__example-image'>
-                <img src="https://cdn.shopify.com/s/files/1/0650/7427/products/fte3011-rose-gold-grey-minimal-ladies-watch_700x.jpg?v=1552765336"
-                  alt="example"
-                  width='100%' />
-              </div>
-            </Col>
-          </Row>
-          
-          <Row>
-            <Col>
-              <h2>New Arrivals</h2>
-            </Col>
-          </Row>
-        </div>
+        <Row>
+          {
+            image.smallImage.map((item) => {
+              return (
+                <Col xs={6} sm={6} md={3}>
+                  <div className='small-image-container'>
+                    <img src={item.url}
+                      alt="example"
+                      width='100%' />
+                  </div>
+                </Col>
+              )
+            })
+          }
+        </Row>
+
+        {/* San pham mau */}
+        <Row>
+          <div className='example-image-container'>
+            <Row>
+              {
+                image.exampleImage.map((item) => {
+                  return (
+                    <Col xs={6} sm={6} md={3}>
+                      <div className='example-image-container__example-image'>
+                        <img src={item.url}
+                          width='100%' />
+                      </div>
+                    </Col>
+                  )
+                })
+              }
+            </Row>
+
+            <Row>
+              <Col>
+                <h2>SẢN PHẨM MỚI</h2>
+              </Col>
+            </Row>
+          </div>
+        </Row>
+        {/* Co-Founder */}
+        <Row>
+          {
+            image.founders.map((item) => {
+              return (
+                <Col sm={12} md={6}>
+                  <Card style={{ width: '100%' }} className='card'>
+                    <Card.Img variant="top" src={item.url} className='card__img' />
+                    <Card.Body>
+                      <Card.Title className="card__title">{item.name}</Card.Title>
+                      <Card.Text>
+                        {item.description}
+                      </Card.Text>
+                      <Button variant="primary">Liên hệ</Button>
+                    </Card.Body>
+                  </Card>
+                </Col>
+              )
+            })
+          }
+        </Row>
       </Container>
     </div>
   )
