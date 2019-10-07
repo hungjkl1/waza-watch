42ba0828afe166d80271731e1d1cf2020cfa345d
hungdtm <hungdtm@pop-os.localdomain>
2019-10-07 10:30:49 +0700

    Navbar non responsive

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 // Page
 import Home from './containers/Home';
--- src/components/NavbarComponent/index.js
+++ src/components/NavbarComponent/index.js
@@ -1,15 +1,40 @@
 import React from 'react';
 import {Nav, Navbar} from 'react-bootstrap';
+import './styles.scss'
 const NavbarComponent = () => {
     return (
-        <Navbar bg="dark" variant="dark">
-        <Navbar.Brand href="#home">Hùng</Navbar.Brand>
-        <Nav className="mr-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#features">Features</Nav.Link>
-          <Nav.Link href="#pricing">Pricing</Nav.Link>
+      <div>
+      <Navbar bg="grey" variant="light">
+      <Nav className='nav-left'>
+        <Nav.Link className='nav-div-center' href="#home"><div>
+          <b>
+          Waza Watch
+          </b>
+      </div></Nav.Link>
         </Nav>
-      </Navbar>
+      <Nav className="ml-auto mr-auto">
+        <Nav.Link className='nav-div-center' href="#home"><div>
+          <img className='logo-nav' src='http://logok.org/wp-content/uploads/2015/03/Hublot-logo-340x214.png'  alt='error'/>
+        </div></Nav.Link>
+        </Nav>
+        <Nav className='nav-right' > 
+        <Nav.Item href="#home" className='ml-auto'>
+        <div>
+        <img className='logo-nav' src='img/cart.png' alt='error'/>
+      </div>
+      </Nav.Item>
+        </Nav>
+    </Navbar>
+      <Nav className="ml-auto nav-bg">
+        <Nav.Link href="#home">Home</Nav.Link>
+        <Nav.Link href="#pricing">Man</Nav.Link>
+        <Nav.Link href="#pricing">Woman</Nav.Link>
+        <Nav.Link href="#pricing">Brands</Nav.Link>
+        <Nav.Link href="#features">Contact Us</Nav.Link>
+        <Nav.Link href="#pricing">Products</Nav.Link>
+
+      </Nav>
+      </div>
     );
 }
 
