645acb0cfedf98b2af31484b37e67a82864b1eb5
hungjkl1 <hungjkl1@gmail.com>
2019-11-28 23:49:56 +0700

    pay by paypal

--- src/containers/ShoppingCart/Form.js
+++ src/containers/ShoppingCart/Form.js
@@ -7,11 +7,16 @@ class FormDelivery extends React.Component {
     super(props);
     this.state = {}
     this.userForm = React.createRef();
+    this.nonUserForm = React.createRef();
+
   }
 
   submitForm=()=>{
-    alert('danger')
     this.userForm.current.dispatchEvent(new Event('submit', { cancelable: true }))
+  }
+
+  submitNonUserForm=()=>{
+    this.nonUserForm.current.dispatchEvent(new Event('submit', { cancelable: true }))
   }
 
   render() {
@@ -49,7 +54,7 @@ class FormDelivery extends React.Component {
 
         {/* Khi người dùng chưa đăng nhập */}
         {!this.props.user &&
-          <Form onSubmit={this.props.handleSubmit}>
+          <Form onSubmit={this.props.handleSubmit} ref={this.nonUserForm}>
             <Form.Group>
               <Form.Label>Họ tên</Form.Label>
               <Form.Control onChange={this.props.handleOnChange} name='name' type="string" />
@@ -78,15 +83,16 @@ class FormDelivery extends React.Component {
             
             <Button className='btn-secondary' variant="primary" type="submit">Thanh toán</Button>
             <PayPalButton
-              amount="0.01"
-              // shippingPreference="NO_SHIPPING" // default is "GET_FROM_FILE"
-              onSuccess={(details, data) => {
-                this.props.getPaypalDetail(details)
-              }}
-              options={{
-                clientId: "PRODUCTION_CLIENT_ID"
-              }}
-            />
+            amount="0.01"
+            // shippingPreference="NO_SHIPPING" // default is "GET_FROM_FILE"
+            onSuccess={(details, data) => {
+              this.props.getPaypalDetail(details)
+              this.submitNonUserForm()
+            }}
+            options={{
+              clientId: "PRODUCTION_CLIENT_ID"
+            }}
+          />
           </Form>
         }
       </div>
